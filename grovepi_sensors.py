c44f69b119e577a234ddfa359783f08620cf3149
rn-1 <rnene@usc.edu>
2025-03-13 16:58:30 -0700

    fix

--- grovepi_sensors.py
+++ grovepi_sensors.py
@@ -25,11 +25,13 @@ while True:
     thresh = grovepi.digitalRead(ultrasonic_ranger) # thresh between 0, 1023
     distance = (grovepi.analogRead(potentiometer)/1023.0)*517 # map to between 0, 517
 
-    obj_pres = (distance < thresh) if "OBJ PRES" else ""
+    obj_pres =  "OBJ PRES" if (distance < thresh) else ""
     setText(f"{thresh}cm {obj_pres}\n{distance}cm")
 
     if(distance < thresh):
       setRGB(255,0,0)
+    else:
+      setRGB(0,255,0)
 
     time.sleep(200)
   
